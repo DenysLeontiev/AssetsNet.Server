2f39adcbca4ba92da95ff4b54a991577d567ea13
Denys Leon <leonden0046@gmail.com>
2024-02-27 18:34:27 +0200

    Added logging  to Register Flow

--- AssetsNet.API/Controllers/AccountController.cs
+++ AssetsNet.API/Controllers/AccountController.cs
@@ -13,26 +13,28 @@ namespace AssetsNet.API.Controllers;
 public class AccountController : BaseApiController
 {
     private readonly IAuthService _authService;
+    private readonly ILogger<AccountController> _logger;
 
-    public AccountController(IAuthService authService)
+    public AccountController(IAuthService authService, ILogger<AccountController> logger)
     {
         _authService = authService;
+        _logger = logger;
     }
 
     [HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(UserJwtDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<UserJwtDto>> RegisterAsync([FromBody] RegisterUserDto registerUserDto)
     {
         try
         {
             var result = await _authService.RegisterAsync(registerUserDto);
+            _logger.LogInformation("User {UserName} registered successfully.", result.UserName);
             return Created("", result);
-        }
-        catch (ArgumentNullException ex)
-        {
-            return NotFound(ex.Message);
         }
         catch (Exception ex)
         {
+            _logger.LogError(ex.Message, "An error occured while registreing new User");
             return BadRequest(ex.Message);
         }
     }
