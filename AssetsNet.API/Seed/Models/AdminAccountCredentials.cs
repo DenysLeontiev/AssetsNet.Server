77d74b7e4ccd78df15f1d57237d6ac76de9bc416
Denys Leon <leonden0046@gmail.com>
2024-02-29 20:11:27 +0200

    Added AdminAccountCredentials

---
+++ AssetsNet.API/Seed/Models/AdminAccountCredentials.cs
@@ -0,0 +1,7 @@
+namespace AssetsNet.API.Seed.Models;
+public class AdminAccountCredentials
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Eamil { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
