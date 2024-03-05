4feb11191b61b444e9f1d76f9ed92593a776b9de
Denys Leon <leonden0046@gmail.com>
2024-03-05 17:40:43 +0200

    Added stock class

--- AssetsNet.API/Controllers/StocksController.cs
+++ AssetsNet.API/Controllers/StocksController.cs
@@ -1,6 +1,7 @@
 using Aletheia.Service;
 using Aletheia.Service.StockData;
 using AssetsNet.API.Controllers.Common;
+using AssetsNet.API.Models.Stock;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AssetsNet.API.Controllers;
@@ -15,11 +16,18 @@ public class StocksController : BaseApiController
     }
 
     [HttpGet("{stockName}")]
-    public async Task<ActionResult> GetStockData([FromRoute] string stockName)
+    public async Task<ActionResult<Stock>> GetStockData([FromRoute] string stockName)
     {
         string aletheiaApiKey = _configuration.GetValue<string>("AletheiaApiKey");
         AletheiaService service = new AletheiaService(aletheiaApiKey);
         StockData quote = await service.GetStockDataAsync(stockName, true, true); // AAPL
+
+        var stock = new Stock(
+            quote.SummaryData.Name,
+            quote.SummaryData.Price.ToString("#,##0.00"),
+            quote.SummaryData.MarketCap.ToString("#,##0"),
+            quote.SummaryData.DollarChange.ToString("#,##0.00"),
+            quote.SummaryData.PercentChange.ToString("#0.0%"));
 
         Console.WriteLine("Name: " + quote.SummaryData.Name);
         Console.WriteLine("Price: " + quote.SummaryData.Price.ToString("#,##0.00"));
@@ -27,6 +35,6 @@ public class StocksController : BaseApiController
         Console.WriteLine("Change $: " + quote.SummaryData.DollarChange.ToString("#,##0.00"));
         Console.WriteLine("Change %: " + quote.SummaryData.PercentChange.ToString("#0.0%"));
 
-        return Ok();
+        return Ok(stock);
     }
 }
--- AssetsNet.API/Models/Stock/Stock.cs
+++ AssetsNet.API/Models/Stock/Stock.cs
@@ -2,9 +2,23 @@ namespace AssetsNet.API.Models.Stock;
 
 public class Stock
 {
+    public Stock(
+        string name,
+        string price,
+        string marketCap,
+        string dollarChange,
+        string percentChange)
+    {
+        Name = name;
+        Price = price;
+        MarketCap = marketCap;
+        DollarChange = dollarChange;
+        PercentChange = percentChange;
+    }
+
     public string Name { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public long MarketCap { get; set; }
-    public decimal DollarChange { get; set; }
-    public decimal PercentChange { get; set; }
+    public string Price { get; set; } = string.Empty;
+    public string MarketCap { get; set; } = string.Empty;
+    public string DollarChange { get; set; } = string.Empty;
+    public string PercentChange { get; set; } = string.Empty;
 }
