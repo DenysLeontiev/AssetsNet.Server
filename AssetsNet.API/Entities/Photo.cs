97dd38c112220fe6b7fd37a0c4edf63396efeaca
Denys Leon <leonden0046@gmail.com>
2024-03-21 16:45:35 +0200

    Added Photo entity

---
+++ AssetsNet.API/Entities/Photo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AssetsNet.API.Entities;
+
+public class Photo
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id { get; set; } = string.Empty;
+    public string PublicId { get; set; } = string.Empty;
+    public string PhotoUrl { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public User? User { get; set; } 
+}
