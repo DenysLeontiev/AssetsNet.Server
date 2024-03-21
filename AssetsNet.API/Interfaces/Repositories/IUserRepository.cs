e0b49d12faca584ce91771ccac5e16c6627c3620
Denys Leon <leonden0046@gmail.com>
2024-03-21 17:35:24 +0200

    Implemented UserRepository

---
+++ AssetsNet.API/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using AssetsNet.API.DTOs.Photo;
+
+namespace AssetsNet.API.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task<Entities.Photo> UploadProfilePhotoDto(UploadProfilePhotoDto uploadProfilePhotoDto);
+}
---
+++ AssetsNet.API/Repositories/User/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using AssetsNet.API.Data;
+using AssetsNet.API.DTOs.Photo;
+using AssetsNet.API.Entities;
+using AssetsNet.API.Interfaces.Photo;
+using AssetsNet.API.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetsNet.API.Repositories.User;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AssetsDbContext _context;
+    private readonly IPhotoService _photoService;
+
+    public UserRepository(AssetsDbContext context, 
+        IPhotoService photoService)
+    {
+        _context = context;
+        _photoService = photoService;
+    }
+    
+    public async Task<Photo> UploadProfilePhotoDto(UploadProfilePhotoDto uploadProfilePhotoDto)
+    {
+        var userToUpdateProfilePhoto = await _context.Users.Include(x => x.ProfilePhoto)
+            .FirstOrDefaultAsync(x => x.Id.Equals(uploadProfilePhotoDto.UserId));
+
+        var photo = await _photoService.UploadAsync(uploadProfilePhotoDto.ProfilePhoto);
+
+        userToUpdateProfilePhoto!.ProfilePhoto = photo;
+
+        await _context.SaveChangesAsync();
+
+        return photo;
+    }
+}
