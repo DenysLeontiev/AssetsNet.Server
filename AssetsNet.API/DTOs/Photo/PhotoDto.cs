18777280bcd8281ce31d11736779e200666cf56f
Andrii <jaworovcki@gmail.com>
2024-03-22 20:37:00 +0200

    Create PhotoDto

---
+++ AssetsNet.API/DTOs/Photo/PhotoDto.cs
@@ -0,0 +1,8 @@
+namespace AssetsNet.API.DTOs.Photo;
+public class PhotoDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string PublicId { get; set; } = string.Empty;
+    public string PhotoUrl { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
