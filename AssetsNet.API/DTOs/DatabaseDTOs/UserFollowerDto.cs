93c032152731aac394edefe9bedf3300be633f7d
Denys Leon <leonden0046@gmail.com>
2024-04-04 18:57:40 +0300

    Added user follower dto

---
+++ AssetsNet.API/DTOs/DatabaseDTOs/UserFollowerDto.cs
@@ -0,0 +1,8 @@
+namespace AssetsNet.API.DTOs.DatabaseDTOs;
+
+public class UserFollowerDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Id { get; set; } = string.Empty;
+    public string PhotoUrl { get; set; } = string.Empty;
+}
