fb5cfecf9cb0c31109ab4c06502fc6dc848e448e
Denys Leon <leonden0046@gmail.com>
2024-04-04 22:17:38 +0300

    Extended DTO

--- AssetsNet.API/DTOs/DatabaseDTOs/UserFollowerDto.cs
+++ AssetsNet.API/DTOs/DatabaseDTOs/UserFollowerDto.cs
@@ -2,7 +2,8 @@ namespace AssetsNet.API.DTOs.DatabaseDTOs;
 
 public class UserFollowerDto
 {
-    public string UserName { get; set; } = string.Empty;
     public string Id { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string PhotoUrl { get; set; } = string.Empty;
 }
