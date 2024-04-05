a4cb9bdfbfa1f6c03ad204acd0cbb254c9c07167
Denys Leon <leonden0046@gmail.com>
2024-04-05 08:53:08 +0300

    Added extension methods

--- AssetsNet.API/ExtensionMethods/ClaimsPrincipalExtensionMethods/ClaimsPrincipalExtensions.cs
+++ AssetsNet.API/ExtensionMethods/ClaimsPrincipalExtensionMethods/ClaimsPrincipalExtensions.cs
@@ -7,24 +7,24 @@ public static class ClaimsPrincipalExtensions
 {
     public static string GetCurrentUserId(this ClaimsPrincipal claimsPrincipal)
     {
-        return GetClaimValueByClaimName(claimsPrincipal, JwtRegisteredClaimNames.NameId);
+        return GetClaimValueByClaimName(claimsPrincipal, ClaimTypes.NameIdentifier);
     }
 
     public static string GetCurrentUserName(this ClaimsPrincipal claimsPrincipal)
     {
-        return GetClaimValueByClaimName(claimsPrincipal, JwtRegisteredClaimNames.GivenName);
+        return GetClaimValueByClaimName(claimsPrincipal, ClaimTypes.Name);
     }
 
     public static string GetCurrentUserEmail(this ClaimsPrincipal claimsPrincipal)
     {
-        return GetClaimValueByClaimName(claimsPrincipal, JwtRegisteredClaimNames.Email);
+        return GetClaimValueByClaimName(claimsPrincipal, ClaimTypes.Email);
     }
 
     private static string GetClaimValueByClaimName(ClaimsPrincipal claimsPrincipal, string claimName)
     {
         var claim = claimsPrincipal.FindFirst(claimName);
 
-        if (claim != null)
+        if (claim is not null)
         {
             return claim.Value;
         }
