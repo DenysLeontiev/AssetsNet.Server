f724ef7f03e975eb4ee73f92dd48015ea99689cf
Denys Leon <leonden0046@gmail.com>
2024-04-11 17:52:15 +0300

    Added migrations

--- AssetsNet.API/Data/20240411145201_AddedMessagingDbArchitecture.Designer.cs
+++ AssetsNet.API/Data/20240411145201_AddedMessagingDbArchitecture.Designer.cs
@@ -4,16 +4,18 @@ using AssetsNet.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace AssetsNet.API.Migrations
+namespace AssetsNet.API.Data
 {
     [DbContext(typeof(AssetsDbContext))]
-    partial class AssetsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240411145201_AddedMessagingDbArchitecture")]
+    partial class AddedMessagingDbArchitecture
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
