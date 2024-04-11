cdfdf18f26a1f0c6fa21cfdb581f7a2d0934d775
Denys Leon <leonden0046@gmail.com>
2024-04-11 18:22:27 +0300

    Added SendMessage method

--- AssetsNet.API/Interfaces/Repositories/IMessageRepository.cs
+++ AssetsNet.API/Interfaces/Repositories/IMessageRepository.cs
@@ -4,5 +4,6 @@ namespace AssetsNet.API.Interfaces.Repositories;
 
 public interface IMessageRepository
 {
+    Task<Message> SendMessage(string currentUserId, string recipientId, string content);
     Task<List<Message>> GetMessages(string senderId, string recipientId); 
 }
--- AssetsNet.API/Repositories/Message/MessageRepository.cs
+++ AssetsNet.API/Repositories/Message/MessageRepository.cs
@@ -13,9 +13,27 @@ public class MessageRepository : IMessageRepository
         _context = context;
     }
 
+
+    public async Task<Entities.Message> SendMessage(string currentUserId, string recipientId, string content)
+    {
+        var message = new Entities.Message
+        {
+            SenderId = currentUserId,
+            RecipientId = recipientId,
+            Content = content,
+            DateSent = DateTime.UtcNow
+        };
+
+        await _context.Messages.AddAsync(message);
+
+        await _context.SaveChangesAsync();
+
+        return message;
+    }
+
     public async Task<List<Entities.Message>> GetMessages(string senderId, string recipientId)
     {
-        var messages = await _context.Messages.Include(x => x.Sender).Include(x => x.Recipient).Where(x => x.SenderId.Equals(senderId) && x.RecipientId.Equals(recipientId) 
+        var messages = await _context.Messages.Include(x => x.Sender).Include(x => x.Recipient).Where(x => x.SenderId.Equals(senderId) && x.RecipientId.Equals(recipientId)
             || (x.SenderId.Equals(recipientId) && x.RecipientId.Equals(senderId)))
                                               .OrderByDescending(x => x.DateSent)
                                               .ToListAsync();
