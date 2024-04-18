f53ad92de798a4d35ac57981580d80c5c0ee1206
Andrii <jaworovcki@gmail.com>
2024-04-18 19:38:14 +0300

    add prices for tariffs

--- AssetsNet.API/Helpers/TariffPlanConsts.cs
+++ AssetsNet.API/Helpers/TariffPlanConsts.cs
@@ -8,5 +8,8 @@ public class TariffPlanConsts
     public const int BasicGptRequests = 100;
 
     public const int PremiumGptRequests = 1000;
-    
+
+    public const decimal BasicPrice = 144.30m;
+
+    public const decimal PremiumPrice = 384.90m;
 }
