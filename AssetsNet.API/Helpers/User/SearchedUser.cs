050d2f65f854b2c63282633e62ad6cfe763fdf2b
Denys Leon <leonden0046@gmail.com>
2024-04-30 19:25:18 +0300

    Added SearchedUser

---
+++ AssetsNet.API/Helpers/User/SearchedUser.cs
@@ -0,0 +1,8 @@
+﻿namespace AssetsNet.API.Helpers.User;
+
+public class SearchedUser
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Id { get; set; } = string.Empty;
+    public string ProfilePhotoUrl { get; set; } = string.Empty;
+}
